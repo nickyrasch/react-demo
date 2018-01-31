7bf55a8fc2f1209f7343078901030b6692bcf73e
Nicky Rasch <nickynrasch@gmail.com>
2018-01-30 16:56:00 -0800

    Remove debugs & add readme

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -29,7 +29,6 @@ class CoursesPage extends React.Component {
   }
 
   render () {
-    debugger;
     return (
       <div>
         <h1>Courses</h1>
@@ -55,7 +54,6 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
-  debugger;
   return {
     courses: state.courses
   };
