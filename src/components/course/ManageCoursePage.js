c895dbff3c1b5709e4d5ae09ead575654f7527ba
Nicky Rasch <nickynrasch@gmail.com>
2018-01-31 03:26:10 -0800

    Persist data on fields for update

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -54,8 +54,20 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course) return course[0]; // since array grab the first one
+  return null; // else fooblaaab nully
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path '/course/:id'
+
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
